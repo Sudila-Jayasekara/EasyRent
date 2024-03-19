61bc1a1e05c35253c7cda2d589726c71fb975db8
Sudila Jayasekara <91514161+Sudila-Jayasekara@users.noreply.github.com>
2024-03-19 10:36:13 +0530

    Database connected

---
+++ backend/index.js
@@ -0,0 +1,22 @@
+import express from "express";
+import { PORT,mongoDBURL } from "./config.js";
+import mongoose from "mongoose";
+
+const app = express();
+
+app.get('/',(request, response) =>{
+    console.log(request)
+    return response.status(234).send('Welcome to ITP Project')
+})
+
+mongoose
+    .connect(mongoDBURL)
+    .then(()=>{
+        console.log('App connected to the database')
+        app.listen(PORT,() =>{
+            console.log(`App is listening to port: ${PORT}`);
+        })
+    })
+    .catch((error)=>{
+        console.log(error);
+    })
