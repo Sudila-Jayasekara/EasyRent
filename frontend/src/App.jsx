2310955fefd122d55806d6c60e79cb8fbddea585
Sudila Jayasekara <91514161+Sudila-Jayasekara@users.noreply.github.com>
2024-03-19 17:33:04 +0530

    add seperate folders for functions

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import {Routes, Route} from 'react-router-dom';
 
 const App = () => {
   return (
-    <div className='bg-red-400 text-white'>EasyRent</div>
+    <Routes>
+      <Route path='' element={} />
+      <Route path='' element={} />
+      <Route path='' element={} />
+      <Route path='' element={} />
+      <Route path='' element={} />
+    </Routes>
   )
 }
 
