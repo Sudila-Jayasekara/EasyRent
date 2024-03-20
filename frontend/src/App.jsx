e189a82b13ae883c9c46a9a2de389b362c0bc711
Dushan Chanuka <chanukadushan1030@gmail.com>
2024-03-21 02:36:39 +0530

    home page updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,13 +3,15 @@ import {Routes, Route} from 'react-router-dom';
 import Layout from './layout/Layout';
 import Home from './pages/Home';
 import Booking from './pages/Booking And Payment Management/Booking';
+import Landing from './pages/Renter Management/Landing';
 
 
 const App = () => {
   return (
     <Routes>
-      <Route path='/' element={<Layout><Home/></Layout>} />
+      <Route path='/home' element={<Layout><Home/></Layout>} />
       <Route path='/booking' element={<Layout><Booking/></Layout>} />
+      <Route path='/' element={<Layout><Landing/></Layout>} />
     </Routes>
   )
 }
