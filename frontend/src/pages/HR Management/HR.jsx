5814420548963f903f54ffda0d70ad384bd34701
Vidumini16 <viduminipavithra@gmail.com>
2024-03-21 11:48:28 +0530

    Create HR Dashboard

---
+++ frontend/src/pages/HR Management/HR.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const HR = () => {
+  return (
+    <div>
+        <h2>
+            Hello HR!
+        </h2>
+        <button>Login</button>
+    </div>
+  )
+}
+
+export default HR
