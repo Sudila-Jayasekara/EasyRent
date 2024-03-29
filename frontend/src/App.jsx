f51b3b7db6bb74839beace63d4222185862c2026
Dushan Chanuka <chanukadushan1030@gmail.com>
2024-03-30 00:55:56 +0530

    updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,12 +3,12 @@ import {Routes, Route} from 'react-router-dom';
 import Layout from './layout/Layout';
 import Home from './pages/Home';
 import Booking from './pages/Booking And Payment Management/Booking';
-import DriverDashboard from './pages/Driver Management/DriverDashboard';
+import DriverDashboard from './pages/Driver Management/DriverdashboardSidebar.jsx';
 import BookingHistory from './pages/Booking And Payment Management/BookingHistory';
 import Payment from './pages/Booking And Payment Management/Payment';
 
 
-main
+
 
 
 const App = () => {
@@ -17,7 +17,7 @@ const App = () => {
       <Route path='/' element={<Layout><Home/></Layout>} />
 
       <Route path='/booking' element={<Layout><Booking/></Layout>} />
-      <Route path='/driver' element={<Layout><Driverdashboard/></Layout>}/>
+      <Route path='/driver' element={<Layout><DriverDashboard/></Layout>}/>
 
       <Route path='/booking'>
         <Route path='' element={<Layout><Booking/></Layout>} />
