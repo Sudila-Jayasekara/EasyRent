8958148dea519068e7c55d5c75327dc310c66eb8
Kaushal <Kaushalsilva15@gmail.com>
2024-03-30 02:27:22 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@ import {Routes, Route} from 'react-router-dom';
 import Layout from './layout/Layout';
 import Home from './pages/Home';
 import Booking from './pages/Booking And Payment Management/Booking';
-import DriverDashboard from './pages/Driver Management/Driverdashboard.jsx';
+import DriverDashboard from './pages/Driver Management/DriverDashboard.jsx';
 import BookingHistory from './pages/Booking And Payment Management/BookingHistory';
 import Payment from './pages/Booking And Payment Management/Payment';
 import DriverdashboardSidebar from './pages/Driver Management/DriverdashboardSidebar.jsx';
