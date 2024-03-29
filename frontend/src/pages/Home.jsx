86f128a2e0230c431cad938a40025c0820c31015
Kaushal <Kaushalsilva15@gmail.com>
2024-03-29 11:54:09 +0530

    build

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,7 +3,39 @@ import { Link } from 'react-router-dom'
 
 function Home() {
   return (
+<<<<<<< Updated upstream
 <div>Home</div>
+=======
+    <div className='flex justify-center space-x-2'>
+        <span className='bg-gray-200 text-black hover:bg-black hover:text-white font-bold py-2 px-4 rounded'>
+          <Link>Renter</Link>
+        </span>
+        <span className='bg-gray-200 text-black hover:bg-black hover:text-white font-bold py-2 px-4 rounded'>
+          <Link>Vehicle</Link>
+        </span>
+       <span className='bg-gray-200 text-black hover:bg-black hover:text-white font-bold py-2 px-4 rounded'>
+          <Link to={'/booking'}>Booking</Link>
+        </span>
+        <span className='bg-gray-200 text-black hover:bg-black hover:text-white font-bold py-2 px-4 rounded'>
+          <Link to={'/driverdashboard'}>Driver</Link>
+        </span>
+        <span className='bg-gray-200 text-black hover:bg-black hover:text-white font-bold py-2 px-4 rounded'>
+          <Link>Owner</Link>
+        </span>
+        <span className='bg-gray-200 text-black hover:bg-black hover:text-white font-bold py-2 px-4 rounded'>
+          <Link>Payment</Link>
+        </span>
+        <span className='bg-gray-200 text-black hover:bg-black hover:text-white font-bold py-2 px-4 rounded'>
+          <Link>HR</Link>
+        </span>
+        <span className='bg-gray-200 text-black hover:bg-black hover:text-white font-bold py-2 px-4 rounded'>
+          <Link>Maintenance</Link>
+        </span>
+        <span className='bg-gray-200 text-black hover:bg-black hover:text-white font-bold py-2 px-4 rounded'>
+          <Link>Reviews</Link>
+        </span>
+    </div>
+>>>>>>> Stashed changes
   )
 }
 
