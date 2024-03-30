43d0b2de5fbe99cb1faf9f87304d7162a32c8054
Nathasha <142737136+Nathasha2001@users.noreply.github.com>
2024-03-30 12:58:34 +0530

    updated

---
+++ frontend/src/pages/Vehicle Management/vehicleSidebar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const vehicleSidebar = () => {
+  return (
+    <>                
+    <button className='bg-white text-black hover:bg-black hover:text-white font-bold py-2 px-4 rounded '>
+    <Link>Approved Forms</Link>
+    </button>
+    <button className='bg-white text-black hover:bg-black hover:text-white font-bold py-2 px-4 rounded'>
+    <Link>Updated Forms</Link>
+    </button>
+    </>
+  )
+}
+
+export default vehicleSidebar
