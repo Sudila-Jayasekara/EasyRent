60b7d968c028ed399b921fb90c5940c32606f36e
Kaushal <Kaushalsilva15@gmail.com>
2024-04-06 23:33:45 +0530

    done

--- frontend/src/pages/Driver Management/DriverDashboard.jsx
+++ frontend/src/pages/Driver Management/DriverDashboard.jsx
@@ -1,26 +1,22 @@
-import React from 'react'
-import thumbnail from '../Driver Management/thumbnail.jpeg'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import thumbnail from '../Driver Management/thumbnail.jpeg';
 
 const DriverDashboard = () => {
   return (
-    <div>
-      <div>
-      <div>
-        <button className='mx-0 bg-black text-white h-16 w-60'  > Check Reserved Dates</button>
+    <div style={{ display: 'grid', gridTemplateColumns: 'repeat(2, 1fr)', gap: '20px' }}>
+      <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'space-between' }}>
+        <button className='bg-black text-white h-16 w-60'>Check Reserved Dates</button>
+        <Link to={'/license'}>
+          <button className='bg-black text-white h-16 w-60 mx-48 my-10'>License Verification</button>
+        </Link>
+        <button className='bg-black text-white h-16 w-60'>Ratings and complaints</button>
       </div>
       <div>
-        <button className='mx-40 bg-black text-white h-16 w-60 my-10'  > License Verification</button>
-        <Link to={'/license'}></Link>
-      </div>
-      <div>
-        <button className='mx-0 bg-black text-white h-16 w-60'  > Ratings and complaints</button>
+        <img className="object-cover w-96 ml-20 h-96 p-1 ring-2 ring-indigo-300 dark:ring-indigo-500" src={thumbnail} alt="Thumbnail" />
       </div>
     </div>
-    <div>
-      <img className="object-cover w-60 h-40 p-1 mx-60 ring-2 ring-indigo-300 dark:ring-indigo-500" src={thumbnail}/>
-    </div>
-    </div>
-  )
-}
+  );
+};
 
-export default DriverDashboard
+export default DriverDashboard;
--- frontend/src/pages/Driver Management/License.jsx
+++ frontend/src/pages/Driver Management/License.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const License = () => {
   return (
     <div>
-        
+        jhfhfgwgfyufweyvuhvhuvuhvguvguvggv
     </div>
      
   )
