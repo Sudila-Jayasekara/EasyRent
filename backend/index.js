7a42d4f78fe1af74284acd591188427e927e1a9e
Sudila Jayasekara <91514161+Sudila-Jayasekara@users.noreply.github.com>
2024-04-07 15:27:17 +0530

    update index

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,22 @@
 import express from "express";
 import { PORT,mongoDBURL } from "./config.js";
 import mongoose from "mongoose";
+import cors from 'cors';
+
+import BookingRoute from './routes/Booking And Payment Management/bookingRoute.js'
 
 const app = express();
+
+//middleware
+app.use(express.json());
+app.use(cors());
 
 app.get('/',(request, response) =>{
     console.log(request)
     return response.status(234).send('Welcome to ITP Project')
 })
 
+app.use('/booking', BookingRoute);
 mongoose
     .connect(mongoDBURL)
     .then(()=>{
