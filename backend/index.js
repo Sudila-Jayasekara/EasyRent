f8b1618230c895f610cfe51548c317b7609baf42
raveesha <it22271150@my.sliit.lk>
2024-04-08 13:20:06 +0530

    updated

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,23 @@
 import express from "express";
+import { PORT,mongoDBURL } from "./config.js";
 import mongoose from "mongoose";
-import { PORT, mongoDBURL } from "./config.js"; // Import PORT and mongoDBURL from config.js
-import router from "./routes/Reviews and rating management/ComplainsRoute.js"; // Import router from ComplainsRoute.js
 
 const app = express();
 
-// Use the router for the "/Complains" route
-app.use("/Complains", router);
+app.get('/',(request, response) =>{
+    console.log(request)
+    return response.status(234).send('Welcome to ITP Project')
+})
 
-mongoose.connect(mongoDBURL)
-  .then(() => console.log('App connected to the database'))
-  .then(() => app.listen(PORT, () => console.log('App is listening to port: ${PORT}')))
-  .catch((error) => console.log(error));
+mongoose
+    .connect(mongoDBURL)
+    .then(()=>{
+        console.log('App connected to the database')
+        app.listen(PORT,() =>{
+            console.log(`App is listening to port: ${PORT}`);
+        })
+    })
+    .catch((error)=>{
+        console.log(error);
+    })
+    
---
+++ frontend/src/components/ComplainsFormD.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ComplainsFormD() {
+  return (
+    <div>
+      hello world
+    </div>
+  )
+}
+
+export default ComplainsFormD
