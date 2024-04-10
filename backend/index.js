d051cdd1c1da140d1853ef9893fce32a45517a62
Dushan Chanuka <chanukadushan1030@gmail.com>
2024-04-11 02:22:50 +0530

    merge from dev

--- backend/index.js
+++ backend/index.js
@@ -2,10 +2,10 @@ import express from "express";
 import { PORT,mongoDBURL } from "./config.js";
 import mongoose from "mongoose";
 import cors from 'cors';
-import cookieParser from 'cookie-parser'
+
 import {authRouter} from "./routes/auth.route.js";
 import { RenterRouter } from './routes/Renter Management/Renter.route.js';
-
+import cookieParser from "cookie-parser";
 import BookingRoute from './routes/Booking And Payment Management/bookingRoute.js'
 
 
@@ -15,6 +15,7 @@ const app = express();
 
 //middleware
 app.use(express.json());
+app.use(cookieParser());
 app.use(cors({
     origin: 'http://localhost:5173',
     credentials: true
--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -114,6 +114,42 @@ router.post('/login', async (req, res, next) => {
         return res.status(500).json({ message: "Internal Server Error" });
     }
 });
-
+// router.post('/forgotpassword',async(req,res)=>{
+//     const{email}=req.body
+//     try{
+//         const renter=await Renter.findOne({email})
+//         if(!renter){
+//             return res.json({
+//                 message:"User Not registered"
+//             })
+//         }
+//     }catch(err){
+//         console.log(err)
+//     }
+//     const token=jwt.sign({id:Renter._id},KEY,{expiresIn:'5m'})
+//     var transporter = nodemailer.createTransport({
+//         service: 'gmail',
+//         auth: {
+//           user: 'chanukadushan130@gmail.com',
+//           pass: 'vsmf xpzq tnwn iipe'
+//         }
+//       });
+      
+//       var mailOptions = {
+//         from: 'EasyRent@gmail.com',
+//         to: email,
+//         subject: 'Reset Password',
+//         text: 'http://localhost:5173/resetPassword/${token}'
+//       };
+      
+//       transporter.sendMail(mailOptions, function(error, info){
+//         if (error) {
+//           return res.json({message:"error while sending"})
+//         } else {
+//           console.log('Email sent: ' + info.response);
+//         }
+//       });
+//   })
+  
 
 export { router as authRouter};
