610082b3c1692831a253c5cc22a82d6a07c12322
Kaushal <Kaushalsilva15@gmail.com>
2024-04-13 03:14:34 +0530

    done

--- frontend/src/pages/Driver Management/DriverDisplay.jsx
+++ frontend/src/pages/Driver Management/DriverDisplay.jsx
@@ -2,61 +2,82 @@ import React from 'react';
 
 const DriverDisplay = () => {
     return (
-        <div className="relative overflow-x-auto">
+        <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
             <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
                 <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                     <tr>
                         <th scope="col" className="px-6 py-3">
-                           Driver name
+                            Product name
                         </th>
                         <th scope="col" className="px-6 py-3">
-                           Driver ID
+                            Color
                         </th>
                         <th scope="col" className="px-6 py-3">
-                           Category
+                            Category
                         </th>
-                        
+                        <th scope="col" className="px-6 py-3">
+                            Price
+                        </th>
+                        <th scope="col" className="px-6 py-3">
+                            <span className="sr-only">Edit</span>
+                        </th>
                     </tr>
                 </thead>
                 <tbody>
-                    <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                    <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
                         <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                            
+                            Apple MacBook Pro 17"
                         </th>
                         <td className="px-6 py-4">
-                           
+                            Silver
                         </td>
                         <td className="px-6 py-4">
-                           
+                            Laptop
                         </td>
-                       
+                        <td className="px-6 py-4">
+                            $2999
+                        </td>
+                        <td className="px-6 py-4 text-right">
+                            <a href="#" className="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
+                        </td>
                     </tr>
-                    <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                    <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
                         <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                            
+                            Microsoft Surface Pro
                         </th>
                         <td className="px-6 py-4">
-                           
+                            White
                         </td>
                         <td className="px-6 py-4">
-                            
+                            Laptop PC
                         </td>
-                        
+                        <td className="px-6 py-4">
+                            $1999
+                        </td>
+                        <td className="px-6 py-4 text-right">
+                            <a href="#" className="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
+                        </td>
                     </tr>
-                    <tr className="bg-white dark:bg-gray-800">
+                    <tr className="bg-white dark:bg-gray-800 hover:bg-gray-50 dark:hover:bg-gray-600">
                         <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                           
+                            Magic Mouse 2
                         </th>
                         <td className="px-6 py-4">
-                            
+                            Black
                         </td>
                         <td className="px-6 py-4">
-                           
+                            Accessories
                         </td>
-                      
+                        <td className="px-6 py-4">
+                            $99
+                        </td>
+                        <td className="px-6 py-4 text-right">
+                            <a href="#" className="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
+                        </td>
                     </tr>
                 </tbody>
             </table>
+            <button className="mt-4 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">Add Drivers</button>
         </div>
     );
 };
