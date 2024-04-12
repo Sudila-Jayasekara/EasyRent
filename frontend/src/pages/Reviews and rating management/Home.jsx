e7a799ecef067b84cacd01f4f484947babbf8587
raveesha <it22271150@my.sliit.lk>
2024-04-12 11:22:28 +0530

    updated

--- frontend/src/pages/Reviews and rating management/Home.jsx
+++ frontend/src/pages/Reviews and rating management/Home.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import axios, { Axios } from 'axios'
-import { Form, Link } from 'react-router-dom' 
+import {Link} from 'react-router-dom';
 
 
 
