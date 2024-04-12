0b0ad9423385e6d68d3c26dcf6f72181b6bed53c
raveesha <it22271150@my.sliit.lk>
2024-04-12 21:01:00 +0530

    updates

--- frontend/src/pages/Reviews and rating management/DeleteComplains.jsx
+++ frontend/src/pages/Reviews and rating management/DeleteComplains.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import React,{useState} from 'react'
+import axios from 'axios'
+import { useNavigate,useParams } from 'react-router-dom'
 
-function DeleteComplains() {
+const DeleteComplains=()=>  {
+
+  const navigate = useNavigate();
+  const { id } = useParams();
+
+const handleDeleteComplains=()=>{
+  axios
+  .delete(`http://localhost:5555/complains/${id}`)
+  .then(()=>{
+    navigate('/');
+})
+  .catch((err)=>{
+    console.log(err);
+        })
+}
+
+
+
   return (
-    <div>
+    <div className='p-4'>
       
+      <h1 className='text-3xl my-4'>Delete Complain</h1>
+      <div className='flex flex-col items-center border-2 border-sky-400 rounded-xl w-[600px] p-8 mx-auto'>
+        <h3 className='text-2xl'>Are You Sure You want to delete this book?</h3>
+      <button
+          className='p-4 bg-red-600 text-white m-8 w-full'
+          onClick={handleDeleteComplains}
+        >
+          yes, delete it
+        </button>
+        </div>
+
     </div>
   )
 }
