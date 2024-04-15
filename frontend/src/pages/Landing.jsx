9571a13872423b8aee0bb06deae7c3f039ccc53a
Nathasha <142737136+Nathasha2001@users.noreply.github.com>
2024-04-16 03:14:52 +0530

    updated

--- frontend/src/pages/Landing.jsx
+++ frontend/src/pages/Landing.jsx
@@ -5,7 +5,7 @@ const Landing = () => {
   
    
       return (
-        <section className="h-screen bg-contain" style={{backgroundImage: "url('https://images.unsplash.com/photo-1532931899774-fbd4de0008fb?q=80&w=2069&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')"}}>
+        <section className="h-screen bg-cover " style={{backgroundImage: "url('https://images.unsplash.com/photo-1532931899774-fbd4de0008fb?q=80&w=2069&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')"}}>
           <div className="flex h-full w-full items-center justify-center container mx-auto px-8">
             <div className="max-w-2xl text-center">
               <h1 className="text-wrap sm:text-5xl capitalize tracking-widest text-blue-700 lg:text-5xl font-semibold">Welcome to EasyRent!</h1>
--- frontend/src/pages/Vehicle Management/Forms.jsx
+++ frontend/src/pages/Vehicle Management/Forms.jsx
@@ -3,29 +3,30 @@ import React from 'react';
 const Forms = () => {
 
     return (
-      <div className="flex h-screen flex-col items-center justify-center space-y-6 bg-gray-100 px-4 sm:flex-row sm:space-x-6 sm:space-y-0">
-        <div className="w-full max-w-sm overflow-hidden rounded-lg bg-white shadow-md duration-300 hover:scale-105 hover:shadow-xl">
-          <svg xmlns="http://www.w3.org/2000/svg" className="mx-auto mt-8 h-16 w-16 text-green-400" viewBox="0 0 20 20" fill="currentColor">
+      <div className="flex h-screen flex-col items-center justify-center space-y-6 bg-gray-200 px-4 sm:flex-row sm:space-x-6 sm:space-y-0">
+        <div className="w-full max-w-sm overflow-hidden rounded-lg bg-yellow-300 shadow-md duration-300 hover:scale-105 hover:shadow-xl">
+          <svg xmlns="http://www.w3.org/2000/svg" className="mx-auto mt-8 h-16 w-16 text-red-600" viewBox="0 0 20 20" fill="currentColor">
             <path fillRule="evenodd" d="M6.267 3.455a3.066 3.066 0 001.745-.723 3.066 3.066 0 013.976 0 3.066 3.066 0 001.745.723 3.066 3.066 0 012.812 2.812c.051.643.304 1.254.723 1.745a3.066 3.066 0 010 3.976 3.066 3.066 0 00-.723 1.745 3.066 3.066 0 01-2.812 2.812 3.066 3.066 0 00-1.745.723 3.066 3.066 0 01-3.976 0 3.066 3.066 0 00-1.745-.723 3.066 3.066 0 01-2.812-2.812 3.066 3.066 0 00-.723-1.745 3.066 3.066 0 010-3.976 3.066 3.066 0 00.723-1.745 3.066 3.066 0 012.812-2.812zm7.44 5.252a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd" />
           </svg>
-          <h1 className="mt-2 text-center text-2xl font-bold text-gray-500">Success</h1>
-          <p className="my-4 text-center text-sm text-gray-500">Woah, successfully completed 3/5 Tasks</p>
-          <div className="space-x-4 bg-gray-100 py-4 text-center">
-            <button className="inline-block rounded-md bg-red-500 px-10 py-2 font-semibold text-red-100 shadow-md duration-75 hover:bg-red-400">Cancel</button>
-            <button className="inline-block rounded-md bg-green-500 px-6 py-2 font-semibold text-green-100 shadow-md duration-75 hover:bg-green-400">Dashboard</button>
+          <h1 className="mt-2 text-center text-2xl font-bold text-gray-500">UPDATED</h1>
+          <p className="my-4 text-center text-2xl text-gray-500 font-medium">Vehicle ID : 05B</p>
+          <div className="space-x-4 bg-gray-800 py-4 text-center">
+            <button className="inline-block rounded-md bg-red-500 px-10 py-2 font-semibold text-red-100 shadow-md duration-75 hover:bg-red-400">VIEW</button>
+            <button className="inline-block rounded-md bg-green-500 px-6 py-2 font-semibold text-green-100 shadow-md duration-75 hover:bg-green-400">UPDATE</button>
           </div>
         </div>
-        <div className="w-full max-w-sm overflow-hidden rounded-lg bg-white shadow-md duration-300 hover:scale-105 hover:shadow-xl">
-          <svg xmlns="http://www.w3.org/2000/svg" className="mx-auto mt-8 h-16 w-16 text-red-500" viewBox="0 0 20 20" fill="currentColor">
-            <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.707 7.293a1 1 0 00-1.414 1.414L8.586 10l-1.293 1.293a1 1 0 101.414 1.414L10 11.414l1.293 1.293a1 1 0 001.414-1.414L11.414 10l1.293-1.293a1 1 0 00-1.414-1.414L10 8.586 8.707 7.293z" clipRule="evenodd" />
+        <div className="w-full max-w-sm overflow-hidden rounded-lg bg-yellow-300 shadow-md duration-300 hover:scale-105 hover:shadow-xl">
+        <svg xmlns="http://www.w3.org/2000/svg" className="mx-auto mt-8 h-16 w-16 text-red-600" viewBox="0 0 20 20" fill="currentColor">
+            <path fillRule="evenodd" d="M6.267 3.455a3.066 3.066 0 001.745-.723 3.066 3.066 0 013.976 0 3.066 3.066 0 001.745.723 3.066 3.066 0 012.812 2.812c.051.643.304 1.254.723 1.745a3.066 3.066 0 010 3.976 3.066 3.066 0 00-.723 1.745 3.066 3.066 0 01-2.812 2.812 3.066 3.066 0 00-1.745.723 3.066 3.066 0 01-3.976 0 3.066 3.066 0 00-1.745-.723 3.066 3.066 0 01-2.812-2.812 3.066 3.066 0 00-.723-1.745 3.066 3.066 0 010-3.976 3.066 3.066 0 00.723-1.745 3.066 3.066 0 012.812-2.812zm7.44 5.252a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd" />
           </svg>
-          <h1 className="mt-2 text-center text-2xl font-bold text-gray-500">Cancel</h1>
-          <p className="my-4 text-center text-sm text-gray-500">Just a small miss, 2/5 Tasks</p>
-          <div className="space-x-4 bg-gray-100 py-4 text-center">
-            <button className="inline-block rounded-md bg-red-500 px-10 py-2 font-semibold text-red-100 shadow-md duration-75 hover:bg-red-400">Cancel</button>
-            <button className="inline-block rounded-md bg-green-500 px-6 py-2 font-semibold text-green-100 shadow-md duration-75 hover:bg-green-400">Try Again</button>
+          <h1 className="mt-2 text-center text-2xl font-bold text-gray-500">UPDATED</h1>
+          <p className="my-4 text-center text-2xl text-gray-500 font-medium">Vehicle ID : 01A</p>
+          <div className="space-x-4 bg-gray-800 py-4 text-center">
+            <button className="inline-block rounded-md bg-red-500 px-10 py-2 font-semibold text-red-100 shadow-md duration-75 hover:bg-red-400">VIEW</button>
+            <button className="inline-block rounded-md bg-green-500 px-6 py-2 font-semibold text-green-100 shadow-md duration-75 hover:bg-green-400">UPDATE</button>
           </div>
         </div>
+        
       </div>
     );
   }
