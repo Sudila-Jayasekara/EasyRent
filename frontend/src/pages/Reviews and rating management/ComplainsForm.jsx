452d93db3c1efc4db02708db22c444fd4708e6a9
raveesha <it22271150@my.sliit.lk>
2024-04-15 23:27:22 +0530

    updated

--- frontend/src/pages/Reviews and rating management/ComplainsForm.jsx
+++ frontend/src/pages/Reviews and rating management/ComplainsForm.jsx
@@ -121,7 +121,7 @@ const ComplainsForm = () => {
                     <button
                         className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline ml-2"
                         type="submit"
-                        onClick={handleButtonClick}
+                        
                     >
                        View your complains
                     </button>
