2d159ac3f431f7bc7f19ee64219613d7b3980506
Dushan Chanuka <chanukadushan1030@gmail.com>
2024-04-16 04:44:51 +0530

    update

--- backend/models/Vehicle Management/vehicleModel.js
+++ backend/models/Vehicle Management/vehicleModel.js
@@ -1,64 +1,63 @@
 import mongoose from "mongoose";
 
 const vehicleSchema = mongoose.Schema({
-    brand: {
-        type: String,
-        required: true
-    },
-    model: {
-        type: String,
-        required: true
-    },
-    owner: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Owner',
-        type: String,
-    },
-    photos: {
-        type: [String],
-    
-    },
-    price: {
-        type: Number,
-        required: true
-    },
-    transmission: {
-        type: String,
-        enum: ['auto', 'manual'],
-        required: true
-    },
-   
-    createdAt: {
-        type: Date,
-        default: Date.now
-    },
-    modelYear:{
-        type: String,
-        required: true
-      },
-      engineCapacity:{
-        type: String,
-        required: true
-      },
-     mileage:{
-        type: String,
-        required: true
-      },
-    
-     totalSeats: {
-        type: Number,
-        required: true
-      },
-      status: {
-        type: String,
-        default: 'Available',
-        enum: ['Available', 'Booked'],
-      },
-      category: {
-        type: String,
-        required: true
-      },
+        brand: {
+                type: String,
+                required: true
+        },
+        model: {
+                type: String,
+                required: true
+        },
+        owner: {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: 'Owner',
+        },
+        photos: {
+                type: [String],
+        
+        },
+        price: {
+                type: Number,
+                required: true
+        },
+        transmission: {
+                type: String,
+                enum: ['auto', 'manual'],
+                required: true
+        },
      
+        createdAt: {
+                type: Date,
+                default: Date.now
+        },
+        modelYear:{
+                type: String,
+                required: true
+            },
+            engineCapacity:{
+                type: String,
+                required: true
+            },
+         mileage:{
+                type: String,
+                required: true
+            },
+        
+         totalSeats: {
+                type: Number,
+                required: true
+            },
+            status: {
+                type: String,
+                default: 'Available',
+                enum: ['Available', 'Booked'],
+            },
+            category: {
+                type: String,
+                required: true
+            },
+         
 
 
 },{timestamps:true});
