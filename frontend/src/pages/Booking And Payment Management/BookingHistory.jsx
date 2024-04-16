e48b57a725ca811fe0779e2cb7779a13e5430668
Sudila Jayasekara <91514161+Sudila-Jayasekara@users.noreply.github.com>
2024-04-16 16:52:42 +0530

    Add @fortawesome/fontawesome-free package and import FontAwesomeIcon in BookingHistory.jsx

--- frontend/src/pages/Booking And Payment Management/BookingHistory.jsx
+++ frontend/src/pages/Booking And Payment Management/BookingHistory.jsx
@@ -2,8 +2,8 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import {Link, useNavigate } from 'react-router-dom';
 
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-// import { faEdit, faTrash } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEdit, faTrash } from '@fortawesome/free-solid-svg-icons';
 
 const BookingHistory = () => {
   const navigate = useNavigate();
