77c10bfe846d7f3c3f71773930ce578031b15e47
raveesha <it22271150@my.sliit.lk>
2024-04-16 17:58:43 +0530

    udated

--- frontend/src/pages/Reviews and rating management/ComplainsForm.jsx
+++ frontend/src/pages/Reviews and rating management/ComplainsForm.jsx
@@ -108,6 +108,7 @@ const ComplainsForm = () => {
                             value={formData.Vehicle_description}
                             onChange={handleChange}
                             required
+
                         />
                     </div>
 
@@ -115,12 +116,14 @@ const ComplainsForm = () => {
                     <button
                         className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline ml-2"
                         type="submit"
+                        
                     >
                         Save
                     </button>
                     <button
                         className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline ml-2"
                         type="submit"
+                        onClick={handleButtonClick}
                         
                     >
                        View your complains
