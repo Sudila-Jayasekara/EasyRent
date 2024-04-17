712ad085095e7b7ed425451c3bfb9f7b76f00ba0
Dushan Chanuka <chanukadushan1030@gmail.com>
2024-04-17 13:16:45 +0530

    updated

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -94,7 +94,7 @@ const Signup = () => {
     const data = await res.json();
     console.log(data);
     if (data.status) {
-      alert("Renter Registered");
+      alert("User Registered");
       navigate("/login");
     }
   };
@@ -262,7 +262,7 @@ const Signup = () => {
             />
           )} */}
           <button
-            className="relative h-10 inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-red-200 via-red-300 to-yellow-200 group-hover:from-red-200 group-hover:via-red-300 group-hover:to-yellow-200 dark:text-white dark:hover:text-gray-900 focus:ring-4 focus:outline-none focus:ring-red-100 dark:focus:ring-red-400 w-full"
+            className="relative h-10 mt-4 inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-red-200 via-red-300 to-yellow-200 group-hover:from-red-200 group-hover:via-red-300 group-hover:to-yellow-200 dark:text-white dark:hover:text-gray-900 focus:ring-4 focus:outline-none focus:ring-red-100 dark:focus:ring-red-400 w-full"
             type="submit"
           >
             Register
