f9217664ae54df7a835e1087d00df2ee5518ecc2
Nathasha <142737136+Nathasha2001@users.noreply.github.com>
2024-04-29 23:31:16 +0530

    updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import axios from 'axios';
 import { Routes, Route } from 'react-router-dom';
+import '@fortawesome/fontawesome-free/css/all.css';
 
 import Layout from './components/Layout';
 
@@ -39,15 +40,15 @@ import Logout from './pages/Logout.jsx';
 import ForgotPassword from './pages/ForgotPassword';
 import ResetPassword from './pages/ResetPassword.jsx';
 
-import HrEmpRegister from './pages/HR Management/HrEmpRegister.jsx';
-import HrPayroll from './pages/HR Management/HrPayroll.jsx';
-import HrDetails from './pages/HR Management/HrDetails.jsx';
-import HrDashboard from './pages/HR Management/HrDashboard.jsx';
-import HrSalaryDetails from './pages/HR Management/HrSalaryDetails.jsx';
-import HrEmpLeave from './pages/HR Management/HrEmpLeave.jsx';
-import HrDetailsEdit from './pages/HR Management/HrDetailsEdit.jsx';
-import HrLeaveDetails from './pages/HR Management/HrLeaveDetails.jsx';
-import HrSalaryEdit from './pages/HR Management/HrSalaryEdit.jsx';
+// import HrEmpRegister from './pages/HR Management/HrEmpRegister.jsx';
+// import HrPayroll from './pages/HR Management/HrPayroll.jsx';
+// import HrDetails from './pages/HR Management/HrDetails.jsx';
+// import HrDashboard from './pages/HR Management/HrDashboard.jsx';
+// import HrSalaryDetails from './pages/HR Management/HrSalaryDetails.jsx';
+// import HrEmpLeave from './pages/HR Management/HrEmpLeave.jsx';
+// import HrDetailsEdit from './pages/HR Management/HrDetailsEdit.jsx';
+// import HrLeaveDetails from './pages/HR Management/HrLeaveDetails.jsx';
+// import HrSalaryEdit from './pages/HR Management/HrSalaryEdit.jsx';
 
 import Displaydates from './pages/Driver Management/Displaydates';
 import Driverprofile from './pages/Driver Management/Driverprofile';
@@ -56,20 +57,19 @@ import DriverDashboard from './pages/Driver Management/Driverdashboard';
 import DriverDisplay from './pages/Driver Management/DriverDisplay';
 import OwnerSidebar from './pages/Vehicle Owner Management/OwnerSidebar.jsx';
 import ViewRenter from './pages/Renter Management/ViewRenter';
-
-import Profile from './pages/Renter Management/Manageprofile.jsx'
-
 import Home from './pages/Reviews and rating management/Home.jsx';
 import ComplainsForm from './pages/Reviews and rating management/ComplainsForm.jsx';
-import VehicleSidebar from './pages/Vehicle Management/VehicleSidebar.jsx';
+import VehicleSidebar from './pages/Vehicle Management/vehicleSidebar.jsx';
 import VehicleManager from './pages/Vehicle Management/vehicleManager.jsx';
 import ApprovedF from './pages/Vehicle Management/ApprovedF.jsx';
 import Forms from './pages/Vehicle Management/Forms.jsx';
-
-import Vprofile from './pages/Vehicle Management/Profile.jsx'
-
-
-import AddedVehicles from './pages/Vehicle Management/AddedVehicles.jsx'
+import Vprofile from './pages/Vehicle Management/Profile.jsx';
+import AddedVehicles from './pages/Vehicle Management/AddedVehicles.jsx';
+import Manageprofile from './pages/Renter Management/Manageprofile';
+import RenterSidebar from './pages/Renter Management/RenterSidebar';
+import RenterHome from './pages/Renter Management/RenterHome';
+import SelectBooking from './pages/Renter Management/SelectBooking';
+import FavouriteList from './pages/Renter Management/FavouriteList.jsx';
 
 
 
@@ -80,6 +80,7 @@ const App = () => {
   return (
     <Routes>
       <Route path="*" element={<Layout><ErrorPage /></Layout>} />
+      <Route path="/favlist" element={<Layout><FavouriteList/></Layout>} />
       <Route path="/" element={<Layout><Landing/></Layout> } />
       <Route path="/landing" element={<Layout><Landing/></Layout>} />
       <Route path="/viewRenter" element={<Layout><ViewRenter/></Layout>} />
@@ -93,44 +94,6 @@ const App = () => {
       <Route path="/selectbooking/:vehicleId" element={<Layout><SelectBooking/></Layout>} />
       <Route path="/signup" element={<Signup />} />
 
-      <Route path='/VehicleManager'>
-        <Route path='' element={<Layout><VehicleManager/></Layout>} />
-      </Route> 
-       <Route path='/VehicleSidebar' element={<Layout><VehicleSidebar/></Layout>}/> 
-      <Route path='/AddedVehicles' element={<Layout><AddedVehicles/></Layout>}/> 
-      <Route path='/Profile' element={<Layout><Vprofile/></Layout>}/>
-      <Route path='/ApprovedF' element={<Layout><ApprovedF/></Layout>}/>
-      <Route path='/Forms' element={<Layout><Forms/></Layout>}/>
-     
-
-
-      <Route path='/landing'element={<Layout><VehicleLand/></Layout>}/>
-      <Route path='/' element={<Layout />} />
-      <Route path='/viewRenter' element={<Layout><ViewRenter/></Layout>}/>
-      <Route path='/login' element={<Login/>} />
-      <Route path='/logout' element={<Logout/>} />
-      <Route path='/forgotpassword' element={<ForgotPassword/>}/>
-      <Route path='/resetPassword' element={<ResetPassword/>}/>
-      <Route path='/landing'element={<Layout><Landing/></Layout>}/>
-      <Route path='/profile'element={<Layout><Manageprofile/></Layout>}/>
-      <Route path='/Rentersidebar'element={<Layout><RenterSidebar/></Layout>}/>
-      <Route path='/homerenter' element={<Layout><RenterHome/></Layout>}/>
-      <Route path='/selectbooking' title="Select Booking" element={<Layout><SelectBooking/></Layout>}/>
-      <Route path='/signup' element={<Signup/>}/>
-
-      
-      <Route path='/booking/create' element={<Layout><CreateBooking/></Layout>}/>
-      <Route path='/booking/history' element={<ShowBookingR/>}/>
-      <Route path='/booking/check' element={<ShowBookingO/>}/>
-
-
-      <Route path='/complains' element={<Home/>}/>
-      <Route path='/complains/details/:id' element={<ShowComplains/>}/>
-      <Route path='/complains/edit/:id' element={<EditComplains/>}/>
-      <Route path='/complains/delete/:id' element={<DeleteComplains/>}/>
-      {/* <Route path='/complainsForm' element={<ComplainsForm/>}/>// create a book */}
-      
-    
       <Route path="/displaydate" element={<Layout><Displaydates/></Layout>} />
       <Route path="/driverprofile" element={<Layout><Driverprofile/></Layout>} />
       <Route path="/license" element={<Layout><License/></Layout>} />
@@ -153,9 +116,10 @@ const App = () => {
       <Route path="/viewvehicle" element={<ViewVehicle/>}/>
       <Route path="/profile" element={<Layout><Vprofile/></Layout>}/>
       <Route path="/AddedVehicles" element={<Layout><AddedVehicles/></Layout>}/>
-      
-      
-      
+      <Route path="/VehicleManager" element={<Layout><VehicleManager/></Layout>}/>
+      <Route path="/VehicleSidebar" element={<Layout><VehicleSidebar/></Layout>}/>
+      <Route path="/ApprovedF" element={<Layout><ApprovedF/></Layout>}/>
+      <Route path="/Forms" element={<Layout><Forms/></Layout>}/>
 
 
       <Route path='/bp' element={<Layout><BookingAndPayment/></Layout>}/>
