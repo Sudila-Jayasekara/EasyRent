7ba34d0fa7e4ccc980660c260f2d335590acdcee
Kaushal <Kaushalsilva15@gmail.com>
2024-04-30 21:16:42 +0530

    do

--- frontend/src/pages/Driver Management/Driverprofile.jsx
+++ frontend/src/pages/Driver Management/Driverprofile.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import React, { useState, useEffect, useRef } from 'react';
 import { useNavigate } from 'react-router-dom';
-// import image from './image.jpeg';
+import image from './image.jpeg';
 
 const Driverprofile = () => {
   const navigate = useNavigate();
