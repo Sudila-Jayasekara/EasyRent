e7f72086bdea07ceae589615a16b9f9034658e77
Kaushal <Kaushalsilva15@gmail.com>
2024-05-03 22:20:39 +0530

    updated the search bar

--- frontend/src/pages/Driver Management/DriverDisplay.jsx
+++ frontend/src/pages/Driver Management/DriverDisplay.jsx
@@ -37,6 +37,33 @@ const DriverDisplay = () => {
     return (
         <div className="container mx-auto ">
             
+            <div>
+            <form className="max-w-md mx-auto" onSubmit={(e) => e.preventDefault()}>
+                <label htmlFor="default-search" className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">
+                    Search
+                </label>
+                <div className="relative">
+                    
+                    <input
+                        type="search"
+                        id="default-search"
+                        className="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                        placeholder="Search drivers."
+                        value={searchTerm}
+                        onChange={(e) => setSearchTerm(e.target.value)}
+                    />
+                    
+                    <button
+                        type="submit"
+                        className="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                        onClick={() => setSearchTerm('')}
+                    >
+                        Search
+                    </button>
+                </div>
+            </form>
+
+            </div>
 
 
          <div className='mt-9'>
